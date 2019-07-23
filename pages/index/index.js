d3cae4b3065b4c4cb41154f99790e50b30f217bc
Peachying <1120025860@qq.com>
2019-07-23 11:19:26 +0800

    initial function

---
+++ pages/index/index.js
@@ -0,0 +1,77 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    title: 'Take care of your posture~',
+    rotation: 0,
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  // bindViewTap: function() {
+  //   wx.navigateTo({
+  //     url: '../logs/logs'
+  //   })
+  // },
+  
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  onReady: function () {
+    var that = this
+    console.log("Always query data")
+    this.timeInterval = setInterval(function(){
+      wx.request({
+        url: 'https://easy-mock.com/mock/5d3669cbad09957385f160dc/example/mock',
+        data: {
+          rotation: ''
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success: function (res) {
+          console.log(res.data)
+          that.setData({
+            rotation: 45
+          })
+        }
+      })
+    }.bind(this), 5000)    
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
